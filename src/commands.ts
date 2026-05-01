7f663065f156f5133533632bfa1bd2d296b705b6
Eris Lietus <yukitoichijo@gmail.com>
2026-05-01 14:34:00 +0100

    handler register

--- src/commands.ts
+++ src/commands.ts
@@ -4,26 +4,19 @@ import { setUser } from "./config";
 export type CommandHandler = (
     cmdname: string,
     ...args:string[]
-) => void; 
+) => Promise<void>; 
 
 export type CommandsRegistry = Record<string, CommandHandler>;
 
-export function handlerLogin(cmdName: string, ...args: string[]){
-if (args.length === 0){
-    throw new Error("Missing user name.")
-}
-setUser(args[0])
-console.log(`user ${args[0]} has been set`)
-};
 
 export function registerCommand(registry: CommandsRegistry, cmdName: string, handler: CommandHandler){
 registry[cmdName] = handler;
 };
 
-export function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]){
-if(!registry[cmdName]){
+export async function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]){
+    if(!registry[cmdName]){
     throw new Error("Invalid Command")
 } else{
-    registry[cmdName](cmdName, ...args)
+   await registry[cmdName](cmdName, ...args)
 }
 };
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { CommandsRegistry, handlerLogin, registerCommand, runCommand } from "./commands";
+import { CommandsRegistry, registerCommand, runCommand } from "./commands";
+import { handlerLogin, handlerRegister} from "./user";
 
-function main() {
+async function  main() {
   const commandsRegistry: CommandsRegistry = {};
   registerCommand(commandsRegistry,"login",handlerLogin)
+  registerCommand(commandsRegistry, "register", handlerRegister);
   const args = process.argv.slice(2);
   if(args.length < 1) {
     console.log("Not enough arguments provided");
@@ -11,14 +13,14 @@ function main() {
     try {
     const cmdName = args[0];
     const cmdArgs = args.slice(1);
-    runCommand(commandsRegistry, cmdName, ...cmdArgs)
+    await runCommand(commandsRegistry, cmdName, ...cmdArgs)
     } catch (error) {
       if (error instanceof Error) {
       console.error(error.message);
 }
       process.exit(1)
     }
-    
+    process.exit(0);
   } 
 }
 
